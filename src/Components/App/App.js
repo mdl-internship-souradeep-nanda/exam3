d1242ae3534406be7aa97ddcbb99d2c797ed14ef
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-21 13:00:06 +0530

    Sidebar structure

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -51,10 +51,12 @@ class App extends React.Component {
     }
     return (
       <div className="App">
-        <Header />
         <SideBar />
-        {centralJsx}
-      </div>
+        <div className="App-body">
+          <Header />
+          {centralJsx}
+        </div>
+      </div >
     );
   }
 }
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import './Sidebar.css';
 function Sidebar() {
   return (
     <div className="Sidebar">
-      Sidebar
+      Bs
     </div>
   );
 }
