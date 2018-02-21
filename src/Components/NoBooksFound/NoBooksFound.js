1116f1678f96c8ee0f4e1a2c5058ca905f58742d
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-21 11:57:57 +0530

    No books found

--- src/Components/NoBooksFound/NoBooksFound.js
+++ src/Components/NoBooksFound/NoBooksFound.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './NoBooksFound.css';
 
+import externals from '../../externals.json';
+
+import refreshImg from './refresh.svg';
+
+import store from '../../store';
+import refresh from '../../Actions/RefreshAction';
+
 function NoBooksFound() {
+  const importBooks = () => {
+    fetch(externals.populateDb).then(() => {
+      store.dispatch(refresh());
+    });
+  };
   return (
     <div className="NoBooksFound">
-      NoBooksFound
+      <div>
+        Oops! No books found!
+      </div>
+      <div>
+        Import them now ?
+      </div>
+      <button
+        onClick={importBooks}
+      >
+        <img src={refreshImg} alt="refresh" />
+      </button>
     </div>
   );
 }
